57c98c11bc89eed81a69e1d167f885f87ca6bb50
Joan <Joanpadolina@gmail.com>
2020-03-04 11:19:47 +0100

    localstorage

--- docs/src/app.js
+++ docs/src/app.js
@@ -1,3 +1,20 @@
 import { routes } from './modules/router';
 
+var inputEmail = document.getElementById("naam");
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', e => {
+
+    localStorage.setItem("naam", inputEmail.value);
+    
+    e.preventDefault()
+    console.log(inputEmail.value)
+    console.log(localStorage)
+
+})
+
+
+
+
 routes();
