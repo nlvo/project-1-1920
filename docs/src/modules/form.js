16c642d39402b38376bee6dda393cb844f32247d
Joan <Joanpadolina@gmail.com>
2020-03-04 16:55:57 +0100

    more chapters

--- docs/src/modules/form.js
+++ docs/src/modules/form.js
@@ -4,12 +4,18 @@ const personClass = document.getElementById('class');
 const teacher = document.getElementById('teacher');
 const dueDate = document.getElementById('due');
 // assignment details
+const tableOfContents = document.getElementById('contents');
 const titleAssignment = document.getElementById('title');
 const bookTitle = document.getElementById('book-title');
 const bookAuthor = document.getElementById('author');
 const bookPublication = document.getElementById('publication');
 const bookIntro = document.getElementById('intro')
-const bookSummary = document.getElementById('summary');
+const chapter1 = document.getElementById('chapter1');
+const chapter2 = document.getElementById('chapter2');
+const chapter3 = document.getElementById('chapter3');
+const chapter4 = document.getElementById('chapter4');
+const chapter5 = document.getElementById('chapter5');
+
 
 let form = document.querySelector('form');
 
@@ -20,12 +26,17 @@ function inputPerson() {
         class: personClass.value,
         teacher: teacher.value,
         due: dueDate.value,
+        contents: tableOfContents.value,
         title: titleAssignment.value,
         bookTitle: bookTitle.value,
         author: bookAuthor.value,
         publication: bookPublication.value,
         intro: bookIntro.value,
-        summary: bookSummary.value
+        chapter1: chapter1.value,
+        chapter2: chapter2.value,
+        chapter3: chapter3.value,
+        chapter4: chapter4.value,
+        chapter5: chapter5.value
     }
     return personData
 }
@@ -54,6 +65,9 @@ function createAssignment() {
         <h2><span>Inleverdatum:</span>${data.due}</h2>
     </article>
     <article class="intro">
+    <p>
+    ${data.contents}
+</p>
         <p>
             ${data.intro}
         </p>
@@ -63,8 +77,20 @@ function createAssignment() {
         <h3>${data.author}</h3>
         <h2>Inleiding</h2>
         <p>
-            ${data.summary}
+            ${data.chapter1}
         </p>
+        <p>
+            ${data.chapter2}
+         </p>
+         <p>
+         ${data.chapter3}
+         </p>
+         <p>
+         ${data.chapter4}
+        </p>
+         <p>
+             ${data.chapter5}
+         </p>
     </article>
 
 </section>
